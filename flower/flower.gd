77f4a0a21e120bef5617d3fdfc8a6c45ef7cc56e
upiny-0 <pierredelaet2000@gmail.com>
2024-11-16 19:22:46 +0100

    coucouuu

--- flower/flower.gd
+++ flower/flower.gd
@@ -1,13 +1,11 @@
 extends Node3D
 
 class Petal:
-	var area : Area3D
+	var rigidbody : RigidBody3D
 	var material : ShaderMaterial
-	var linear_velocity := Vector3.ZERO
-	var angular_velocity := Vector3.ZERO
 	
-	func _init(_area : Area3D, _material : ShaderMaterial) -> void:
-		area = _area
+	func _init(_rigidbody : RigidBody3D, _material : ShaderMaterial) -> void:
+		rigidbody = _rigidbody
 		material = _material
 
 var last_petal_selected : Petal
@@ -21,38 +19,54 @@ func _ready() -> void:
 		if child is MeshInstance3D && child.name.contains("petals"):
 			(child as MeshInstance3D).set_surface_override_material(0, child.get_active_material(0).duplicate())
 			
-			var petal_area := Area3D.new()
-			add_child(petal_area)
-			petal_area.global_position = child.global_position
-			#petal_area.set_collision_layer_value(1, false)
-			petal_area.set_collision_mask_value(1, false)
-			child.reparent(petal_area)
+			var petal_rb := RigidBody3D.new()
+			add_child(petal_rb)
+			petal_rb.global_position = child.global_position
+			petal_rb.set_collision_layer_value(1, true)
+			petal_rb.set_collision_layer_value(2, false)
+			petal_rb.set_collision_mask_value(1, false)
+			petal_rb.set_collision_mask_value(2, true)
+			petal_rb.freeze = true
+			petal_rb.mass = 0.005
+			petal_rb.gravity_scale = 0.75
+			petal_rb.continuous_cd = true
+			petal_rb.angular_damp = 10.0
+			child.reparent(petal_rb)
 			
 			var col_shape := CollisionShape3D.new()
-			col_shape.shape = ConcavePolygonShape3D.new()
-			col_shape.shape.set_faces(child.mesh.get_faces())
+
+			(child as MeshInstance3D).create_convex_collision(false)
+			col_shape.shape = (child.get_child(0).get_child(0) as CollisionShape3D).shape.duplicate()
+			child.get_child(0).queue_free()
 			
-			petal_area.add_child(col_shape)
-			col_shape.global_position = petal_area.global_position
+			petal_rb.add_child(col_shape)
+			col_shape.global_position = petal_rb.global_position
 			
-			var petal := Petal.new(petal_area, (child as MeshInstance3D).get_surface_override_material(0))
+			var petal := Petal.new(petal_rb, (child as MeshInstance3D).get_surface_override_material(0))
 			
-			petal_area.mouse_entered.connect(_on_petal_mouse_entered.bind(petal))
-			petal_area.mouse_exited.connect(_on_petal_mouse_exited.bind(petal))
+			petal_rb.mouse_entered.connect(_on_petal_mouse_entered.bind(petal))
+			petal_rb.mouse_exited.connect(_on_petal_mouse_exited.bind(petal))
 
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("click"):
 		if last_petal_selected != null:
+			GameManager.camera.enable_rotation = false
 			grabbed_petal = last_petal_selected
 			grabbed_petal.material.set_shader_parameter("grab_mouse_position", Vector2(get_viewport().get_mouse_position()) / Vector2(get_viewport().size))
-	if Input.is_action_just_released("click") && grabbed_petal != null:
-		var tween := create_tween()
-		tween.set_trans(Tween.TRANS_ELASTIC)
-		tween.set_ease(Tween.EASE_OUT)
-		tween.tween_method(_tween_deform_parameter.bind(grabbed_petal), deform, Vector2.ZERO, 0.5)
-		deform = Vector2.ZERO
-		grabbed_petal = null
+	if Input.is_action_just_released("click"):
+		GameManager.camera.enable_rotation = true
+		
+		if grabbed_petal != null:
+			var tween := create_tween()
+			tween.set_trans(Tween.TRANS_ELASTIC)
+			tween.set_ease(Tween.EASE_OUT)
+			tween.tween_method(_tween_deform_parameter.bind(grabbed_petal), deform, Vector2.ZERO, 0.75)
+			deform = Vector2.ZERO
+			grabbed_petal = null
+	
+	if event is InputEventMouseMotion && last_petal_selected != null:
+		last_petal_selected.material.set_shader_parameter("grab_mouse_position", Vector2(event.position) / Vector2(get_viewport().size))
 	
 	if event is InputEventMouseMotion && grabbed_petal != null:
 		deform += Vector2(event.relative.x, -event.relative.y) / Vector2(get_viewport().size) * 0.1
@@ -60,12 +74,23 @@ func _input(event: InputEvent) -> void:
 		
 		if deform.length() > 0.03:
 			var tween := create_tween()
-			tween.set_trans(Tween.TRANS_ELASTIC)
+			tween.set_trans(Tween.TRANS_BOUNCE)
 			tween.set_ease(Tween.EASE_OUT)
-			tween.tween_method(_tween_deform_parameter.bind(grabbed_petal), deform, Vector2.ZERO, 0.5)
+			tween.tween_method(_tween_deform_parameter.bind(grabbed_petal), deform, Vector2.ZERO, 0.75)
 			deform = Vector2.ZERO
-			grabbed_petal.linear_velocity = (grabbed_petal.area.global_position - global_position) * Vector3(20.0, 10.0, 20.0)
-			grabbed_petal.angular_velocity = Vector3(randf() * 2.0 - 1.0, randf() * 2.0 - 1.0, randf() * 2.0 - 1.0)
+			grabbed_petal.rigidbody.freeze = false
+			
+			var camera = get_viewport().get_camera_3d()
+			var origin = camera.project_ray_origin(event.position);
+			var direction = camera.project_ray_normal(event.position);
+			var plane = Plane(0.0, 1.0, 0.0, grabbed_petal.rigidbody.global_position.y);
+			var point = plane.intersects_ray(origin, direction);
+			var delta : Vector3 = (grabbed_petal.rigidbody.global_position - Vector3.UP * 0.1) - point
+			
+			grabbed_petal.rigidbody.apply_impulse(
+				-delta * Vector3(1.0, 3.0, 1.0) * 0.01,
+				 Vector3.ONE * 0.5
+				)
 			petals_fallen.append(grabbed_petal)
 			grabbed_petal = null
 
@@ -74,17 +99,9 @@ func _tween_deform_parameter(progress : Vector2, petal : Petal):
 	petal.material.set_shader_parameter("deform", progress)
 
 
-func _physics_process(delta: float) -> void:
-	for petal in petals_fallen:
-		petal.linear_velocity += Vector3.DOWN * delta * 9.81
-		petal.area.global_position += petal.linear_velocity * delta * 0.1
-		
-		petal.area.global_rotation += petal.angular_velocity * delta
-
-
 func _on_petal_mouse_entered(petal : Petal):
 	last_petal_selected = petal
-	petal.material.set_shader_parameter("emission", Color.DARK_SLATE_GRAY)
+	petal.material.set_shader_parameter("emission", Color("#7F3636"))
 
 
 func _on_petal_mouse_exited(petal : Petal):
--- phone/camera_scene.gd
+++ phone/camera_scene.gd
@@ -2,11 +2,11 @@ extends Node3D
 class_name RotatableCamera
 
 @export var camera_radius: float = 10.0
+@export var camera_target: Vector3 = Vector3.ZERO
 
-var rotation_enabled := true
+var enable_rotation := true
 
-var camera_sensitivity: float = 0.01
-var camera_target: Vector3 = Vector3.ZERO
+var camera_sensitivity: float = 0.1
 var mouse_button_pressed_at_previous_frame: bool = false
 var previous_mouse_position: Vector2
 
@@ -20,12 +20,8 @@ var min_longitude: float = - PI / 3
 var max_longitude: float = PI / 3
 
 
-func _ready() -> void:
-	rotate_camera(longitude, latitude)
-
-
-func _process(_delta: float):
-	if !rotation_enabled:
+func _process(delta: float):
+	if !enable_rotation:
 		return
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
@@ -34,19 +30,20 @@ func _process(_delta: float):
 			mouse_button_pressed_at_previous_frame = true
 			previous_mouse_position = current_mouse_position
 		else:
-			var camera_displacement: Vector2 = current_mouse_position - previous_mouse_position
+			var camera_displacement: Vector2 = (current_mouse_position - previous_mouse_position) * delta
 			longitude = clamp(longitude + camera_displacement.x * camera_sensitivity, min_longitude, max_longitude)
 			latitude = clamp(latitude - camera_displacement.y * camera_sensitivity, min_latitude, max_latitude)
-			rotate_camera(longitude, latitude)
 			previous_mouse_position = current_mouse_position
 	else:
 		mouse_button_pressed_at_previous_frame = false
-
-
-func rotate_camera(new_longitude: float, new_latitude):
-	var x: float = camera_radius * cos(new_latitude) * cos(new_longitude)
-	var y: float = camera_radius * sin(new_latitude)
-	var z: float = - camera_radius * cos(new_latitude) * sin(new_longitude)
 	
-	global_position = Vector3(x, y, z)
+	rotate_camera(delta)
+
+
+func rotate_camera(delta : float):
+	var x: float = camera_radius * cos(latitude) * cos(longitude)
+	var y: float = camera_radius * sin(latitude)
+	var z: float = - camera_radius * cos(latitude) * sin(longitude)
+	
+	global_position = Vector3(x, y, z).lerp(global_position, exp(-5.0 * delta))
 	look_at(camera_target)

66aa363f2fd2bb429ba2e490f3c25f36469364a0
EliotNiedercorn <niedercorn.eliot@gmail.com>
2024-11-16 13:05:15 +0100

    Camera improvement and telephone

--- phone/camera_scene.gd
+++ phone/camera_scene.gd
@@ -3,14 +3,17 @@ extends Node3D
 var camera_sensitivity: float = 0.01
 var camera_target: Vector3 = Vector3.ZERO
 var mouse_button_pressed_at_previous_frame: bool = false
-var previous_mouse_position := Vector2.ZERO
+var previous_mouse_position: Vector2
 
-var camera_radius: float = 5.0
+var camera_radius: float = 10.0
 var longitude: float = 0.0  # Initial longitude in radians
 var latitude: float = PI / 3  # Initial latitude in radians
 
-var min_latitude: float = PI / 8  # Minimum latitude
-var max_latitude: float = 1.5 / 2  # Maximum latitude (~80 degrees)
+## Boundaries to camera rotation
+var min_latitude: float = PI / 8
+var max_latitude: float = (PI / 2) - 0.01
+var min_longitude: float = - PI / 3
+var max_longitude: float = PI / 3
 
 
 func _ready() -> void:
@@ -25,7 +28,7 @@ func _process(_delta: float):
 			previous_mouse_position = current_mouse_position
 		else:
 			var camera_displacement: Vector2 = current_mouse_position - previous_mouse_position
-			longitude += camera_displacement.x * camera_sensitivity
+			longitude = clamp(longitude + camera_displacement.x * camera_sensitivity, min_longitude, max_longitude)
 			latitude = clamp(latitude - camera_displacement.y * camera_sensitivity, min_latitude, max_latitude)
 			rotate_camera(longitude, latitude)
 			previous_mouse_position = current_mouse_position
@@ -34,9 +37,9 @@ func _process(_delta: float):
 
 
 func rotate_camera(new_longitude: float, new_latitude):
-	var x = camera_radius * cos(latitude) * cos(longitude)
-	var y = camera_radius * sin(latitude)
-	var z = - camera_radius * cos(latitude) * sin(longitude)
+	var x: float = camera_radius * cos(new_latitude) * cos(new_longitude)
+	var y: float = camera_radius * sin(new_latitude)
+	var z: float = - camera_radius * cos(new_latitude) * sin(new_longitude)
 	
 	global_position = Vector3(x, y, z)
 	look_at(camera_target)
